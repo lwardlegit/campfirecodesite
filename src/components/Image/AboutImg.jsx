7db62bd3d1a44689f4f921604d30d1330e4c7d87
lwardlegit <webcraftapps@gmail.com>
2021-08-08 17:58:31 -0400

    sixth

--- src/components/Image/AboutImg.jsx
+++ src/components/Image/AboutImg.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import Img from '../../images/fire.png';
-import { withPrefix } from 'gatsby'
+
 
 console.log(Img)
 function AboutImg(){
-  return <img src={withPrefix(Img)} alt="about image" height="450px" width="350px"/>
+  return <img src={Img} alt="about image" height="450px" width="350px"/>
 }
 
 export default AboutImg;
